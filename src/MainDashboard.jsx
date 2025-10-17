6d6fd0fddf8a216e80101148d24c16da1e3b7fc1
Attilio Santana Minzoni <hiluaa7@gmail.com>
2025-10-17 14:06:18 -0300

    .

--- src/MainDashboard.jsx
+++ src/MainDashboard.jsx
@@ -260,7 +260,7 @@ export default function MainDashboard() {
                         </a>
                       )}
                     </div>
-                    <div>
+                    <div className="flex gap-3 mt-4 items-end">
                       <button
                         onClick={() => handleReagendarConsulta(consulta, idx)}
                         className="self-end mt-2 px-4 py-1 rounded bg-blue-100 text-blue-700 font-semibold text-sm hover:bg-blue-200 transition-colors"
